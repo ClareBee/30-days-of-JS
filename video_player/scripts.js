01b1bb769b89389b3bd558ab20c075a4e2a7da1f
Clare <chblackburne@gmail.com>
2018-11-21 22:09:49 +0000

    Toggle play and pause

---
+++ video_player/scripts.js
@@ -0,0 +1,22 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateBtn() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+toggle.addEventListener('click', togglePlay);
