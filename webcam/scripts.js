a370e438e75f71f9f6b8a291c1cb4a7a224493a8
Clare <chblackburne@gmail.com>
2018-11-28 20:44:22 +0000

    manipulates pixels to create filter effects

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -21,7 +21,15 @@ function paintToCanvas(){
   canvas.width = width;
   canvas.height = height;
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height)
+    ctx.drawImage(video, 0, 0, width, height);
+    // get pixels
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them :)
+    pixels = rgbSplit(pixels);
+    // pixels = greenScreen(pixels);
+    ctx.globalAlpha = 0.1;
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -36,6 +44,40 @@ function takePhoto(){
   link.setAttribute('download', 'handsome');
   link.innerHTML = `<img src="${data}" alt="Handsome human"/>`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function rgbSplit(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4){
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
 }
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
